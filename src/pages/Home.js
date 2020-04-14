581537a538c4e5551698581f2aaad6086b2fa464
Akshit Thakur <akshit.235@gmail.com>
2020-04-14 19:42:05 +0530

    hero added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,18 @@
 import React from "react";
-
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 export default function Home() {
-  return <div>Hello from home</div>;
+  return (
+    <Hero>
+      <Banner
+        title="Luxurious Rooms"
+        subtitle="Delux Rooms starting at Rs.3000"
+      >
+        <Link to="/rooms" className="btn-primary">
+          Our rooms
+        </Link>
+      </Banner>
+    </Hero>
+  );
 }
